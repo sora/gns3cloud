79d71ecc33fa8fa6775797b613ce39242d625163
sora <sora@haeena.net>
2019-05-30 00:18:21 +0900

    deploy for multiple hosts

--- deploy-gns3vm/deploy.py
+++ deploy-gns3vm/deploy.py
@@ -1,68 +1,112 @@
-import yaml, os, sys, time
+import os, sys, time
+import subprocess
 import shutil
 
 from jinja2 import Template, Environment, FileSystemLoader
+import yaml
 
-config_file = 'site.yaml'
+class Config:
+    def __init__(self):
+        #self.working_dir = '/tmp/gns3cloud-%d-%f' % (os.getpid(), time.time())
+        self.working_dir = './run'
+        print(self.working_dir)
 
-virt_xml = 'gns3.xml'
-cloud_init_base = 'base.yaml'
-cloud_init_network = 'network.yaml'
+    def open(self, config_file='site.yaml'):
+        vm_list = dict()
+        with open(config_file, 'r') as site_file:
+            site_data = yaml.safe_load(site_file)
+            vm_list = site_data['vm_list']
+        return vm_list
 
-template_virt_dir = 'virt'
-template_cloud_init_dir = 'cloud-init'
+    def make_work_dir(self, work_dir=''):
+        if work_dir == '':
+            work_dir = self.working_dir
+        try:
+            os.mkdir(work_dir)
+        except Exception as e:
+            print("cannot make the working directory")
+            print(e)
+            sys.exit(1)
 
+    def make_output_dir(self, vmid):
+        out_dir = "%s/%s" % (self.working_dir, vmid)
+        try:
+            os.mkdir(out_dir)
+        except Exception as e:
+            print("cannot make the output directory")
+            print(e)
+            sys.exit(1)
+        return out_dir
 
-def make_work_dir(work_dir):
-    try:
-        os.mkdir(work_dir)
-    except Exception as e:
-        print("cannot make the working directory")
-        print(e)
-        sys.exit(1)
+    # tsukattenai
+    def remove_work_dir(self):
+        try:
+            shutil.rmtree(self.working_dir)
+        except:
+            print("cannot remove the working directory")
+            sys.exit(1)
 
-def make_output_dir(out_dir):
-    try:
-        os.mkdir(out_dir)
-    except Exception as e:
-        print("cannot make the output directory")
-        print(e)
-        sys.exit(1)
+    def build_deploy_file(self, out_dir, settings):
+        env = Environment(loader = FileSystemLoader('.'))
+        template = env.get_template("%s/%s" % (settings['dir'], settings['file']))
+        output = template.render(settings['setting'])
+        with open("%s/%s" % (out_dir, settings['file']), 'w') as f:
+            f.write(output)
 
-def remove_work_dir(work_dir):
-    try:
-        shutil.rmtree(work_dir)
-    except:
-        print("cannot remove the working directory")
-        sys.exit(1)
+    def build_deploy_file_all(self, vm_list):
+        for vm_id, vm_config in vm_list.items():
+            output_dir = self.make_output_dir(vm_id)
+            for config in vm_config.items():
+                config_name = config[0]
+                settings = config[1]
+                if config_name == 'qemu':
+                    self.build_deploy_qemu(output_dir, settings)
+                else:
+                    self.build_deploy_file(output_dir, settings)
 
-def build_deploy_file(work_dir, input_dir, input_file, config):
-    env = Environment(loader = FileSystemLoader('.'))
-    template = env.get_template("%s/%s" % (input_dir, input_file))
-    output = template.render(config)
-    with open("%s/%s" % (work_dir, input_file), 'w') as f:
-        f.write(output)
+    def build_deploy_qemu(self, out_dir, settings):
+        print('qemu')
+
+        os_img_path = settings['setting']['os_image_path']
+        qcow2_disk = settings['setting']['qcow2_disk_name']
+        disk_size = settings['setting']['disk_gb']
+
+        if os.path.isfile(os_img_path):
+            print("os image file doesn't exist: " + os_img_path)
+            sys.exit(1)
+
+        # command
+        out_path = "%s/%s" % (out_dir, qcow2_disk)
+        try:
+            result = subprocess.check_call([
+                "qemu-img",
+                "convert",
+                "-O qcow2",
+                os_img_path,
+                out_path ])
+        except:
+            print("command faild: qemu-img convert")
+            sys.exit(1)
+
+        # command
+        try:
+            result = subprocess.check_call([
+                "qemu-img",
+                "resize",
+                out_path,
+                disk_size ])
+        except:
+            print("command faild: qemu-img resize")
+            sys.exit(1)
 
 def main():
-    working_dir = '/tmp/gns3cloud-%d-%f' % (os.getpid(), time.time())
+    config = Config()
+    config.make_work_dir()
 
-    make_work_dir(working_dir)
+    vm_list = config.open('site.yaml')
 
-    vm = dict()
+    config.build_deploy_file_all(vm_list)
 
-    with open(config_file, "r") as site_file:
-        site_data = yaml.safe_load(site_file)
-        vm = site_data['vm_list']
-
-    for vm_id, vm_config in vm.items():
-        output_dir = "%s/%s" % (working_dir, vm_id)
-
-        make_output_dir(output_dir)
-        build_deploy_file(output_dir, template_virt_dir, virt_xml, vm_config)
-        build_deploy_file(output_dir, template_cloud_init_dir, cloud_init_base, vm_config)
-        build_deploy_file(output_dir, template_cloud_init_dir, cloud_init_network, vm_config)
-
-    #remove_work_dir(working_dir)
 
 if __name__ == "__main__":
     main()
