5198b584ea9e182577a7232ba00304e3dc9a449f
sora <sora@haeena.net>
2019-06-03 11:50:18 +0900

    fix

--- deploy-gns3vm/deploy.py
+++ deploy-gns3vm/deploy.py
@@ -9,26 +9,26 @@ def main():
         print("Make working directory ...")
         gns3.make_work_dir()
 
-    #print("Downloading images ...")
-    #gns3.download_images()
+    print("Downloading images ...")
+    gns3.download_images()
 
-    #print("Delete all running VMs ...")
-    #gns3.delete_vm_all(vm_list)
+    print("Delete all running VMs ...")
+    gns3.delete_vm_all(vm_list)
 
-    #print("Remove output directory ...")
-    #gns3.remove_output_dir_all(vm_list)
+    print("Remove output directory ...")
+    gns3.remove_output_dir_all(vm_list)
 
-    #print("Make output directory ...")
-    #gns3.make_output_dir_all(vm_list)
+    print("Make output directory ...")
+    gns3.make_output_dir_all(vm_list)
 
-    #print("Build config files ...")
-    #gns3.build_deploy_file_all(vm_list)
+    print("Build config files ...")
+    gns3.build_deploy_file_all(vm_list)
 
-    #print("Build vm images ...")
-    #gns3.build_vm_image_all(vm_list)
+    print("Build vm images ...")
+    gns3.build_vm_image_all(vm_list)
 
-    #print("Deploy all VMs ...")
-    #gns3.deploy_vm_all(vm_list)
+    print("Deploy all VMs ...")
+    gns3.deploy_vm_all(vm_list)
 
 if __name__ == "__main__":
     main()
--- deploy-gns3vm/setup.bash
+++ deploy-gns3vm/setup.bash
@@ -1,6 +1,6 @@
 # packages
 echo "Install all packages for GNS3cloud"
-apt install -y qemu-kvm libvirt-bin virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top
+apt install -y qemu-kvm libvirt-bin virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top libvirt-dev
 apt install -y unzip cloud-image-utils cloud-init 
 apt install -y python3-jinja2 python3-libvirt python3-yaml
 
