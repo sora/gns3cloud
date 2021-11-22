5da2a79c36ad05180b2ac28a6c7465dbeb20b34e
sora <sora@haeena.net>
2021-11-22 18:07:03 +0900

    updates the vmm setup script

--- deploy-gns3vm/setup.bash
+++ deploy-gns3vm/setup.bash
@@ -1,12 +1,11 @@
 # packages
 echo "Install all packages for GNS3cloud"
-apt install -y qemu-kvm libvirt-bin virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top libvirt-dev
+apt install -y qemu-kvm libvirt-clients libvirt-daemon-system virtinst bridge-utils libosinfo-bin libguestfs-tools virt-top libvirt-dev
 apt install -y unzip cloud-image-utils cloud-init 
 apt install -y python3-jinja2 python3-libvirt python3-yaml
 
-# delete default bridge device
+## delete default bridge device
 echo "Remove default bridge device: virbr0
 virsh net-autostart default --disable
 virsh net-destroy default
-
 
